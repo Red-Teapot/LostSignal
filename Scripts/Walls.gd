d92edad1fb9ae4151af8586de5fb806f1f7ab550
Nikita Medvedev <some.redteapot@gmail.com>
2020-08-29 22:42:04 +0700

    Remove some old files

--- Scripts/Walls.gd
+++
@@ -1,4 +0,0 @@
-extends TileSet
-tool
-
-func 
--- Scripts/WallsTileset.gd
+++
@@ -1,8 +0,0 @@
-extends TileSet
-tool
-
-func _is_tile_bound(drawn_id, neighbor_id):
-	if neighbor_id == 1:
-		return true
-	else:
-		return neighbor_id in get_tiles_ids()

bd0d16c822b9ab6fc86854cfdaed42034fedc09d
Nikita Medvedev <some.redteapot@gmail.com>
2020-09-13 21:45:33 +0700

    Implement new UI theme, clean more stuff up

--- Scripts/Level.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _enter_tree():
-	SoundPlayer.play('mainTheme')
-	find_node('FadeRect').visible = true
-	find_node('FadeAnims').play("FadeIn")
-
-func fadeOut():
-	find_node('FadeAnims').play("FadeOut")
--- Scripts/PlayButton.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _pressed():
-	SoundPlayer.play('click')
-	LevelInfo.startFirstLevel()
